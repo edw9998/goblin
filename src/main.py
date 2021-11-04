064de4839673c08476da4b3c1bdb3877a3d1567d
Jason Christian H <nakamarujc@gmail.com>
2021-11-04 19:03:03 +0700

    Added scuffed menu

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 from db import getDb
+from state import state
 
+while True:
+  print("Welcome to goblin! Select your menu")
+  print("1. Register")
+  print("2. Login")
+  # Do Log out
+  print("3. Select Topic")
+  input()
