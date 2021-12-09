1aeb9cd50be45e8f1f664d52478b20b0be411ee4
Jason Christian H <nakamarujc@gmail.com>
2021-12-09 18:07:02 +0700

    Completed forum API

--- src/backend/routes/forum.py
+++ src/backend/routes/forum.py
@@ -8,13 +8,28 @@ forum_bl = flask.Blueprint("forum", __name__)
 def get_topics():
   return flask.jsonify(api.get_topics())
 
-# <id> is an argument
-@forum_bl.route("/topics/<id>")
-def get_topic(id):
+@forum_bl.route("/topics/<id>/message", methods=["GET"])
+def get_messages(id):
+  """Gets all the messages associated with the topic id specified
+
+  Args:
+      id (str): Topic id
+  """
   return flask.jsonify(api.get_messages_topic(id))
 
 @forum_bl.route("/topics", methods=["POST"])
 def create_topic():
   req = request.get_json(force=True)
   api.create_topic(req["topic"], req["id"])
+  return "Success!"
+
+@forum_bl.route("/topics/<id>/message", methods=["POST"])
+def create_message(id):
+  """Creates a new message in the specified topic by `id`
+
+  Args:
+      id (str): Topic id
+  """
+  req = request.get_json(force=True)
+  api.add_message_topic(id, req["userId"], req["msg"])
   return "Success!"
